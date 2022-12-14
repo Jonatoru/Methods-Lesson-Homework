16acc95e111a4a5a4a357d1156ac843c2d64f3f9
Jonatoru <jetrence95@gmail.com>
2022-12-14 22:17:57 +0500

    Решил задачу 1

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,23 @@
 public class Main {
+    public static void definitionOfLeapYear(int year){
+        int leapYear = 4;
+        int noLeapYear = 100;
+        int leapYear400 = 400;
+        if (year % noLeapYear == 0 && year % leapYear400 == 0) {
+            System.out.println(year + " год является високосным.");
+        } else if (year % leapYear == 0 && year % noLeapYear != 0) {
+            System.out.println(year + " год является високосным.");
+        } else if (year % noLeapYear == 0) {
+            System.out.println(year + " год не является високосным.");
+        } else {
+            System.out.println(year + " год не является високосным.");
+        }
+    }
     public static void main(String[] args) {
+
+        System.out.println("Задача 1");
+
+        int year = 2022;
+        definitionOfLeapYear (year);
     }
 }
